8df5936866a8f7c7cae4c0c117c8e44279dcb705
Antonin-M <antonin.mougel1@etu.univ-lorraine.fr>
2026-03-19 08:14:18 +0100

    Debut refactoring A completer plus tard

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -7,10 +7,43 @@ class GildedRose {
         this.items = items;
     }
 
+    public void doAgedBrie(Item i) {
+        //TODO
+    }
+
+    public void doBackstagePass(Item i) {
+        //TODO
+    }
+
+    public void doSulfura(Item i) {
+        //TODO
+    }
+
+    public void doDefault(Item i) {
+        items[i].quality = items[i].quality - 1;
+    }
+
     public void updateQuality() {
+        for (Item i : items) {
+            switch (i.name) {
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    doBackstagePass(i);
+                    break;
+                case "Aged Brie":
+                    doAgedBrie(i);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    doSulfura(i);
+                    break;
+                default:
+                    doDefault(i);
+                    break;
+            }
+        }
+
+
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
